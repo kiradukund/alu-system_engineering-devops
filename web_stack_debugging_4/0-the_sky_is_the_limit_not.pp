48f935f94d8db063d2303eff04084af8e7bb0c24
kiradukund <k.iradukund@alustudent.com>
2026-07-15 03:28:44 -0700

    Fix nginx ULIMIT using init.d restart

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,11 @@
 # Fixes Nginx to handle high load by increasing ULIMIT open files limit
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/:/sbin/',
 }
 
-exec { 'fix-worker-connections':
-  command => 'sed -i "s/worker_connections 768/worker_connections 4096/" /etc/nginx/nginx.conf',
+exec { 'restart-nginx':
+  command => '/etc/init.d/nginx restart',
   path    => '/usr/local/bin/:/bin/:/sbin/',
-}
-
-exec { 'reload-nginx':
-  command => 'nginx -s reload',
-  path    => '/usr/local/bin/:/bin/:/sbin/',
-  require => [Exec['fix--for-nginx'], Exec['fix-worker-connections']],
+  require => Exec['fix--for-nginx'],
 }
