a8b28c8395af3846501eadb6cd6305f923fda271
kiradukund <k.iradukund@alustudent.com>
2026-07-15 03:00:21 -0700

    Fix nginx ULIMIT with exact string replacement

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# Fixes Nginx to handle high load by increasing the ULIMIT
+# Fixes Nginx to handle high load by increasing the ULIMIT open files
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && nginx -s reload',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && nginx -s reload',
   path    => '/usr/local/bin/:/bin/:/sbin/',
 }
