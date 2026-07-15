e07653814d76081d26a8d1fe0e458cda80892f07
kiradukund <k.iradukund@alustudent.com>
2026-07-15 03:25:25 -0700

    Fix nginx ULIMIT and worker connections for high load

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# Fixes Nginx to handle high load by increasing the ULIMIT open files
+# Fixes Nginx to handle high load by increasing ULIMIT and worker connections
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && nginx -s reload',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sed -i "s/worker_connections 768/worker_connections 4096/" /etc/nginx/nginx.conf && sed -i "s/worker_connections  768/worker_connections  4096/" /etc/nginx/nginx.conf && nginx -s reload',
   path    => '/usr/local/bin/:/bin/:/sbin/',
 }
