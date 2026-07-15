be06f36dcc3b3c55838ff36f698d80e48b76c3f4
kiradukund <k.iradukund@alustudent.com>
2026-07-15 03:27:01 -0700

    Fix nginx high load with separate exec resources

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,16 @@
-# Fixes Nginx to handle high load by increasing ULIMIT and worker connections
+# Fixes Nginx to handle high load by increasing ULIMIT open files limit
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sed -i "s/worker_connections 768/worker_connections 4096/" /etc/nginx/nginx.conf && sed -i "s/worker_connections  768/worker_connections  4096/" /etc/nginx/nginx.conf && nginx -s reload',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/:/sbin/',
+}
+
+exec { 'fix-worker-connections':
+  command => 'sed -i "s/worker_connections 768/worker_connections 4096/" /etc/nginx/nginx.conf',
+  path    => '/usr/local/bin/:/bin/:/sbin/',
+}
+
+exec { 'reload-nginx':
+  command => 'nginx -s reload',
+  path    => '/usr/local/bin/:/bin/:/sbin/',
+  require => [Exec['fix--for-nginx'], Exec['fix-worker-connections']],
 }
